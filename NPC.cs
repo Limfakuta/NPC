506685bbd907e9cd75f1e102322e2bc3863a73f5
Limfakuta <olesiakushnereva@gmail.com>
2024-07-05 19:34:53 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //Здоровье NPC
+    public int health = 5;
+    //Уровень NPC
+    public int level = 1;
+    //Скорость NPC
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print("Жизни:" + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Позиция NPC
+        Vector3 newPosition = transform.position;
+        //По оси Z
+        newPosition.z += speed * Time.deltaTime;
+        //Меняет
+        transform.position = newPosition;
+    }
+}
---
+++ NPCUp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCUp : MonoBehaviour
+{
+    //Здоровье NPC
+    public int health = 5;
+    //Уровень NPC
+    public int level = 1;
+    //Скорость NPC
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print("Жизни:" + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Позиция NPC
+        Vector3 newPosition = transform.position;
+        //По оси Y
+        newPosition.y += speed * Time.deltaTime;
+        //Меняет
+        transform.position = newPosition;
+    }
+}
